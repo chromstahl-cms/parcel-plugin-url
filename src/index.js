72712cd16adbdebf1454e53ed9220f403e3b2688
fred <fredlahde@gmail.com>
2019-04-24 13:09:54 +0200

    First draft

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const fs = require('file-system');
+const path = require('path');
+
+const getUrl = prod => {
+    const configFileExists = fs.existsSync("api.json");
+    let url = undefined;
+
+    if (configFileExists) {
+        const data = fs.readFileSync("api.json", "UTf-8");
+        const urlObject = JSON.parse(data)["url"];
+        if (prod) {
+            url = urlObject["prod"];
+        } else {
+            url = urlObject["dev"];
+        }
+    }
+
+    const envUrl = process.env.CHROMSTAHL_URL;
+
+    if (envUrl != undefined) {
+        url = envUrl;
+    }
+
+    return url;
+};
+
+const writeURLFile = url => {
+    const content = `
+export default const url = "${url}";
+`;
+
+    fs.writeFileSync("src/url.ts", content);
+};
+
+module.exports = bundler => {
+    bundler.on('buildStart', async files => {
+        const prod = process.env.NODE_ENV;
+        const url = getUrl(prod);
+        writeURLFile(url);
+    });
+};
